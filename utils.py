7c575785d5c7fa736083ce20da539431275f1a1f
mavaldivie <mavaldivie98@gmail.com>
2022-10-29 00:25:15 -0400

    Fixed plot_graphs method to show all learning curves regirstered

--- utils.py
+++ utils.py
@@ -12,46 +12,27 @@ def plot_graphs(history, smooth = False):
             else:
                 smoothed_points.append(point)
         return smoothed_points
+
+    for key, value in history.items():
+        if key.startswith('val'): continue
+
+        training = smooth_curve(value) if smooth else value
+        validation = smooth_curve(history[f'val_{key}']) if smooth else history[f'val_{key}'] 
+        epochs = range(1, len(training) + 1)
+        try:
+            plt.figure()
+            plt.plot(epochs, training, 'r', label=f'Training {key}')
+            plt.plot(epochs, validation, 'b', label=f'Validation {key}')
+            plt.title(f'Training and validation {key}')
+            plt.legend()
+        except: 
+            pass
    
-    acc = smooth_curve(history['binary_accuracy']) if smooth else history['binary_accuracy']
-    val_acc = smooth_curve(history['val_binary_accuracy']) if smooth else history['val_binary_accuracy']
-    loss = smooth_curve(history['loss']) if smooth else history['loss']
-    val_loss = smooth_curve(history['val_loss']) if smooth else history['val_loss']
-    dice = smooth_curve(history['dice_coef']) if smooth else history['dice_coef']
-    val_dice = smooth_curve(history['val_dice_coef']) if smooth else history['val_dice_coef']
-    jacc = smooth_curve(history['jacc_coef']) if smooth else history['jacc_coef']
-    val_jacc = smooth_curve(history['val_jacc_coef']) if smooth else history['val_jacc_coef']
-    epochs = range(1, len(acc) + 1)
-
-    plt.plot(epochs, acc, 'bo', label='Training acc')
-    plt.plot(epochs, val_acc, 'b', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    
-    plt.figure()
-    plt.plot(epochs, loss, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-
-    plt.figure()
-    plt.plot(epochs, dice, 'bo', label='Training dice')
-    plt.plot(epochs, val_dice, 'b', label='Validation dice')
-    plt.title('Training and validation dice coefficients')
-    plt.legend()
-
-    plt.figure()
-    plt.plot(epochs, jacc, 'bo', label='Training Jaccard')
-    plt.plot(epochs, val_jacc, 'b', label='Validation Jaccard')
-    plt.title('Training and validation Jaccard coefficients')
-    plt.legend()
-
     plt.show()
 
-    print('Max Validation Acc:', max(val_acc))
-    print('Min Validation Loss:', min(val_loss))
-    print('Max Validation Dice:', max(val_dice))
-    print('Max Validation Jaccard:', max(val_jacc))
+    for key, value in history.items():
+        if not key.startswith('val'): continue
+        print(f'Min/Max Validation {key}', min(value), max(value))
 
 def save_dict_as_json(data, file_name):
     with open(file_name + ".json", "w") as fp:
